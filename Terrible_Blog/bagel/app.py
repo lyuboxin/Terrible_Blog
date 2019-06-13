ca8bb4d8d7e4aa147328ba8ee0944d08300bfaa8
Boxin Lyu <lyuboxin@gmail.com>
2019-06-13 17:46:18 -0400

    git ignore

--- Terrible_Blog/bagel/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-app = Flask("terrible_blog")
-app.secret_key = 'terribleblog'
-CORS(app)
--- Terrible_Blog/bagel/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-def get_timestamp(t=None):
-    dt = t if t else datetime.utcnow()
-    return dt.isoformat() + 'Z'
-
